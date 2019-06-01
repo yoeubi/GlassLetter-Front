acc1029f23a91b75ca4ddfde24e07619cb0feade
JungwonSeong <yoeubi28@gmail.com>
2019-06-01 17:31:21 +0900

    init scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import classNames from "classnames/bind";
+import styles from "./App.module.scss";
+
+const cx = classNames.bind(styles);
 
 class App extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route path="/" render={() => <div>hello world</div>} />
+          <Route
+            path="/"
+            render={() => <div className={cx("app")}>hello world</div>}
+          />
         </Switch>
       </div>
     );
