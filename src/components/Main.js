0a0c00ccac73af4bc070fdd6908f09328c2f39ae
samslow <shsvkdldj@likelion.org>
2019-06-02 04:28:37 +0900

    Stabilizing
    Signed-off-by: samslow <shsvkdldj@likelion.org>

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,14 +4,13 @@ import classNames from "classnames/bind";
 import styles from "./Main.module.scss";
 import {sendtext,sendmultipart} from "../request/sendMessage"
 import { Auth } from "aws-amplify";
-<<<<<<< HEAD
 import "react-quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
-=======
 import Navbar from "./Navbar";
->>>>>>> 7de22f1c5d5cf645d6baa049e89c64a9fa748018
+import axios from 'axios';
 
 const cx = classNames.bind(styles);
+
 
 const Main = () => {
   const [name, setName] = useState("");
@@ -73,7 +72,6 @@ const Main = () => {
       </div>
       <div className={cx("text-wrapper")}>
         <form action="" onSubmit={onSubmit}>
-<<<<<<< HEAD
           <ReactQuill
             onChange={onChange}
             value={text}
@@ -81,18 +79,13 @@ const Main = () => {
             formats={formats}
           />
           <button className={cx("plus")}>유리병 등록하기</button>
-=======
-         <input type="file" name="myfile"></input>
-          <textarea onChange={e => setText(e.target.value)} value={text} />
-          <button>등록</button>
->>>>>>> 7de22f1c5d5cf645d6baa049e89c64a9fa748018
         </form>
       </div>
       {popup && (
         <div className={cx("popup-wrapper")}>
           <div className={cx("popup")}>
             <div className={cx("title")}>
-              옵션 추가하기{" "}
+              연락처 추가하기{" "}
               <span className={cx("exit")} onClick={() => setPopup(false)}>
                 ✖
               </span>
