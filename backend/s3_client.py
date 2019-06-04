a4f566bae216751a8af13f81db91f47908a79299
ghk829 <ghk829@hanmail.net>
2019-06-04 16:00:48 +0900

    Update s3_client.py

--- backend/s3_client.py
+++ backend/s3_client.py
@@ -2,9 +2,9 @@ from flask import request
 class S3Client:
 
     def __init__(self,**kwargs):
-        self.ACCESS_KEY_ID = kwargs.get("access_key_id","AKIAZQQLHJM3VYTL2LWR")
-        self.ACCESS_KEY_PASSWD = kwargs.get("access_key_passwd", "PMLUCrm67HGygjGEOqD0uFna14UlvXOOGTNOM5h1")
-        self.BUCKET_NAME = kwargs.get("bucket_name", "glassletter")
+        self.ACCESS_KEY_ID = kwargs.get("access_key_id","")
+        self.ACCESS_KEY_PASSWD = kwargs.get("access_key_passwd", "")
+        self.BUCKET_NAME = kwargs.get("bucket_name", "")
         self.REGION_NAME = kwargs.get("region_name","ap-northeast-2")
         self.request = kwargs.get("request")
 
