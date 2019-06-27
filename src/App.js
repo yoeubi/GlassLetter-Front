8954766d413a9b4380d98cd40927cda6fbc486ea
JungwonSeong <yoeubi28@gmail.com>
2019-06-27 21:49:17 +0900

    set to local

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import { withAuthenticator } from "aws-amplify-react";
 import Splash from "./components/Splash";
-import withSplash from "./hoc/withSplash";
+// import withSplash from "./hoc/withSplash";
 import Main from "./components/Main";
 import GlassWill from "./components/GlassWill";
 import SignUp from "./components/SignUp";
